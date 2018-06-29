0099cad0d62a96291003a1eee83b9b73819a5031
Evgeniy Kilimchuk <ekilimchuk@yandex.ru>
2018-06-29 11:38:16 +0300

    Irregular verbs app.

---
+++ irregular-verbs/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+  "os"
+  "path/filepath"
+  "io/ioutil"
+)
+
+type Data struct {
+  url string // url
+  path string // filepath
+}
+
+func printHelp() {
+  fmt.Printf("Use: ./%s <run|fetch>\n", filepath.Base(os.Args[0]))
+}
+
+func runAppFetch(d Data) {
+  err := d.Fetch()
+  fmt.Printf("Fetch %v\n", err)
+}
+
+func runApp() {
+  fmt.Println("Run")
+}
+
+func (d Data) Exist() bool {
+  fmt.Println(d.path)
+  return true
+}
+
+func (d Data) Fetch() error {
+  res, err := http.Get(d.url)
+  if err != nil {
+    return err
+  }
+  body, err := ioutil.ReadAll(res.Body)
+  defer res.Body.Close()
+  if err != nil {
+    return err
+  }
+  fmt.Printf("%s\n", body)
+  return err
+}
+
+func main()  {
+  s1 := "native"
+  s3 := "ru"
+  s2 := "english"
+
+  data := Data{
+    fmt.Sprintf("https://www.%s-%s.%s/grammar/irregular-verbs", s1, s2, s3),
+    "./data.txt",
+  }
+
+  if len(os.Args) < 2 {
+    printHelp()
+    os.Exit(1)
+  }
+
+  switch os.Args[1] {
+  case "run":
+    runApp()
+  case "fetch":
+    runAppFetch(data)
+  default:
+    printHelp()
+  }
+  os.Exit(0)
+}
