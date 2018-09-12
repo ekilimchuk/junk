dd56d5e94988fe1dab7a9db17da66d5ff83e08fd
Evgeny Kilimchuk <ekilimchuk@yandex-team.ru>
2018-09-12 14:54:41 +0300

    Catch collision.

--- robot-name/robotname.go
+++ robot-name/robotname.go
@@ -1,35 +1,26 @@
 package robotname
 
 import (
+	"fmt"
 	"math/rand"
-	"time"
 )
+
+const MAX = 676000 //int(math.Pow(26.0, 2.0) * math.Pow(10.0, 3.0))
 
 type Robot struct {
 	ID string
 }
 
-func getRandChar() byte {
-	rand.Seed(time.Now().UnixNano())
-	return byte(rand.Intn(int('Z'-'A'+1)) + 'A')
+func getRandChar() rune {
+	return rune(rand.Intn(int('Z'-'A'+1)) + 'A')
 }
 
-func getRandNumb() byte {
-	rand.Seed(time.Now().UnixNano())
-	return byte(rand.Intn(int('9'-'0'+1)) + '0')
+func getRandNumb() rune {
+	return rune(rand.Intn(int('9'-'0'+1)) + '0')
 }
 
-func getRandBytes() []byte {
-	bytes := make([]byte, 5)
-	for i, _ := range bytes {
-		switch {
-		case i < 2:
-			bytes[i] = getRandChar()
-		default:
-			bytes[i] = getRandNumb()
-		}
-	}
-	return bytes
+func getRandString() string {
+	return fmt.Sprintf("%c%c%c%c%c", getRandChar(), getRandChar(), getRandNumb(), getRandNumb(), getRandNumb())
 }
 
 func (r *Robot) Reset() {
@@ -41,11 +32,16 @@ func (r *Robot) Reset() {
 func (r *Robot) Name() string {
 	d := NewData()
 	if r.ID == "" {
+		collision := 0
 		for {
-			r.ID = string(getRandBytes())
+			r.ID = getRandString()
 			if !d.ExistKey(r.ID) {
 				d.AddKey(r.ID)
 				break
+			}
+			collision++
+			if collision >= MAX {
+				panic("IDs overflow")
 			}
 		}
 	}
