03384bb7cee758f380c9a213a038d4012cc4f643
Evgeniy Kilimchuk <ekilimchuk@yandex.ru>
2018-09-13 19:31:56 +0300

    Add comments.

--- series/series.go
+++ series/series.go
@@ -1,16 +1,19 @@
 package series
 
+// All returns all the contiguous substrings.
 func All(n int, s string) (ret []string) {
-	for i, _ := range s[:len(s)+1-n] {
+	for i := range s[:len(s)+1-n] {
 		ret = append(ret, s[i:i+n])
 	}
 	return ret
 }
 
+// UnsafeFirst returns a first the contiguous substring.
 func UnsafeFirst(n int, s string) string {
 	return s[:n]
 }
 
+// First returns a first the contiguous substring with a safe mode.
 func First(n int, s string) (string, bool) {
 	if len(s) < n {
 		return "", false
