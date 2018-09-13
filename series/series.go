aeef3f349acd71314f3fa5bfc97fff54bb3e87f9
Evgeny Kilimchuk <ekilimchuk@yandex-team.ru>
2018-09-13 17:01:00 +0300

    Add the series package.

---
+++ series/series.go
@@ -0,0 +1,20 @@
+package series
+
+func All(n int, s string) []string {
+	var ret []string
+	for i, _ := range s[:len(s)+1-n] {
+		ret = append(ret, s[i:i+n])
+	}
+	return ret
+}
+
+func UnsafeFirst(n int, s string) string {
+	return s[:n]
+}
+
+func First(n int, s string) (string, bool) {
+	if len(s) < n {
+		return "", false
+	}
+	return s[:n], true
+}
