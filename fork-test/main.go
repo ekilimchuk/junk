2552cfe2bf170052ed8b4436e44cd5d3d75a1c64
Kilimchuk Evgeniy Viktorovich <ekilimchuk@ozon.ru>
2025-03-31 18:07:05 +0300

    Fix paths.

--- fork-test/main.go
+++ fork-test/main.go
@@ -3,16 +3,19 @@ package main
 import (
 	"log"
 	"math"
+	"os"
 	"os/exec"
 )
 
 func main() {
+	pwd, _ := os.Getwd()
+
 	for i := uint64(0); i <= math.MaxUint64; i++ {
 		if i > math.MaxInt {
 			log.Printf("Int overflow!\n")
 		}
 		log.Printf("Iter %v\n", i)
-		cmd := exec.Command("./script.sh")
+		cmd := exec.Command(pwd + "/script.sh")
 		if err := cmd.Run(); err != nil {
 			log.Fatal(err)
 		}
--- fork-test/start.sh
+++ fork-test/start.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-telegraf_user="telegraf"
 logfile="./start.log"
 pidfile="./start.pid"
+path=$PWD
+telegraf_user=$USER
 
 /usr/sbin/daemon -fcr -P ${pidfile} -u ${telegraf_user} -o ${logfile} \
-    go run main.go
+    go run ${path}/main.go
