524528b6a91201d0da42fcb30f607fcfe5ec0e42
Kilimchuk Evgeniy Viktorovich <ekilimchuk@ozon.ru>
2025-04-01 16:22:58 +0300

    Add workpool.

--- fork-test/main.go
+++ fork-test/main.go
@@ -1,23 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"math"
 	"os"
 	"os/exec"
 )
 
-func main() {
+func worker(n uint32, results chan<- string) {
 	pwd := os.Getenv("PWD")
-
 	for i := uint64(0); i <= math.MaxUint64; i++ {
 		if i > math.MaxInt {
-			log.Printf("Int overflow!\n")
+			results <- fmt.Sprintf("Worker %v int overflow!\n")
 		}
-		log.Printf("Iter %v\n", i)
+		results <- fmt.Sprintf("Worker %v iter %v\n", n, i)
 		cmd := exec.Command(pwd + "/script.sh")
 		if err := cmd.Run(); err != nil {
 			log.Fatal(err)
 		}
+	}
+}
+
+func main() {
+	workersCount := uint32(1024)
+	results := make(chan string)
+	for n := uint32(0); n <= workersCount; n++ {
+		go worker(n, results)
+	}
+	for {
+		log.Printf("%v\n", <-results)
 	}
 }
